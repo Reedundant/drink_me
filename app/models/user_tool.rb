ddb483fcfdccbbee09f197ed14fd7efcb15f8624
Etienne Forcet <eforcet@icloud.com>
2022-11-22 17:56:48 -0500

    edited user tool model

---
+++ app/models/user_tool.rb
@@ -0,0 +1,5 @@
+class UserTool < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :tool
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_221044) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_224233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_221044) do
     t.index ["user_id"], name: "index_user_ingredients_on_user_id"
   end
 
+  create_table "user_tools", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tool_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tool_id"], name: "index_user_tools_on_tool_id"
+    t.index ["user_id"], name: "index_user_tools_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_221044) do
 
   add_foreign_key "user_ingredients", "ingredients"
   add_foreign_key "user_ingredients", "users"
+  add_foreign_key "user_tools", "tools"
+  add_foreign_key "user_tools", "users"
 end
