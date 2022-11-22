906b8ceca103ebc02a0eec391a851d3f08556c22
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-22 16:10:51 -0500

    Define associations between ingredient, user_ingredient, and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :user_ingredients
+  has_many :ingredients, through: :user_ingredients
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
