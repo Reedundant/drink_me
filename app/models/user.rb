db6c7b00972753644ee99dc7a2e897bc64d8b83a
Etienne Forcet <eforcet@icloud.com>
2022-11-22 16:06:14 -0500

    created user model/created migration/db migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :ingredients, through: :user_ingredients
+  has_many :tools, through: :user_tools
+
+  validates :username, presence: true
+  validates :age, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
