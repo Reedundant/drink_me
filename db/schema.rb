f7205c5d3b89ecdaad39b61d603b4790c8265653
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-22 16:28:17 -0500

    Rails db:migrate, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_205634) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_ingredients", force: :cascade do |t|
+    t.bigint "ingredient_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_user_ingredients_on_ingredient_id"
+    t.index ["user_id"], name: "index_user_ingredients_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_205634) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_ingredients", "ingredients"
+  add_foreign_key "user_ingredients", "users"
 end
