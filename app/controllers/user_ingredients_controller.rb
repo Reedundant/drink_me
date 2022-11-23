9a772b8d6d11da0c7f7076ce2403b12542a85c85
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-23 14:32:31 -0500

    Set form for ingredient selection

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,2 +1,11 @@
 class UserIngredientsController < ApplicationController
+  def new
+    @user_ingredient = UserIngredient.new
+    @ingredients_test = Ingredient.all
+    @liquors = Ingredient.where(category: 'liquor')
+    @liqueurs = Ingredient.where(category: 'liqueur')
+    @syrups = Ingredient.where(category: 'syrup')
+    @juices = Ingredient.where(category: 'juice')
+    # raise
+  end
 end
