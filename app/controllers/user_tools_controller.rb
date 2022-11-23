166cb33cc5633a0f28854b388a6f0ac2726db82a
Etienne Forcet <eforcet@icloud.com>
2022-11-23 17:43:57 -0500

    created user tool controller, route, view

---
+++ app/controllers/user_tools_controller.rb
@@ -0,0 +1,26 @@
+class UserToolsController < ApplicationController
+
+  def new
+    @user = current_user
+    @user_tool = UserTool.new
+  end
+
+  def create
+    @user = current_user
+    @tools = Tool.where(id: params[:user_tool][:tool_id])
+
+    @tools.each do |tool|
+      @user_tool = UserTool.new(user: @user, tool: tool)
+      render :new, status: 422 and return unless @user_tool.save
+    end
+
+    redirect_to my_tools_path(@inmate.prison)
+
+  end
+
+  private
+
+  def user_tool_params
+    params.require(:user_tool).permit(:name, )
+  end
+end
