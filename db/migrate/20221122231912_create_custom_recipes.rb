68c20be5e07febca93e607b575737af7c6511ae4
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-23 11:10:24 -0500

    COMMENT OUT recipe column for the since model doesn't exist yet and for the sake of being able to migrate and continue on working on the user_ingredients controller

--- db/migrate/20221122231912_create_custom_recipes.rb
+++ db/migrate/20221122231912_create_custom_recipes.rb
@@ -1,7 +1,7 @@
 class CreateCustomRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :custom_recipes do |t|
-      t.references :recipe, null: false, foreign_key: true
+      # t.references :recipe, null: false, foreign_key: true
 
       t.timestamps
     end
