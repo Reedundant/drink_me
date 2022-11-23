426f5836656519a100fe680931babcfcd1b6fbd3
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-22 22:09:10 -0500

    Generate Ingredient instances to add to the ingredients table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "> Destroying all ingredients..."
+Ingredient.destroy_all
+puts "> Done destroying all ingredients! "
+puts "> Generating ingredients..."
+puts ""
+
+# Liquors
+# rum, gin, scotch, cognac, whisky, vodka, vadka, tequila
+Ingredient.create(name: 'rum', category: 'liquor')
+Ingredient.create(name: 'gin', category: 'liquor')
+Ingredient.create(name: 'scotch', category: 'liquor')
+Ingredient.create(name: 'cognac', category: 'liquor')
+Ingredient.create(name: 'whisky', category: 'liquor')
+Ingredient.create(name: 'vodka', category: 'liquor')
+Ingredient.create(name: 'vadka', category: 'liquor')
+Ingredient.create(name: 'tequila', category: 'liquor')
+
+#Liqueurs
+# coffee liqueur, amaro, herbal liqueur, sweet vermouth, dry vermouth, aperitif, cointreau
+Ingredient.create(name: 'coffee liqueur', category: 'liqueur')
+Ingredient.create(name: 'amaro', category: 'liqueur')
+Ingredient.create(name: 'herbal liqueur', category: 'liqueur')
+Ingredient.create(name: 'sweet vermouth', category: 'liqueur')
+Ingredient.create(name: 'dry vermouth', category: 'liqueur')
+Ingredient.create(name: 'aperitif', category: 'liqueur')
+Ingredient.create(name: 'cointreau', category: 'liqueur')
+
+# Syrups
+Ingredient.create(name: 'honey', category: 'syrup')
+Ingredient.create(name: 'maple syrup', category: 'syrup')
+
+# Juices
+Ingredient.create(name: 'lemon juice', category: 'juice')
+Ingredient.create(name: 'lime juice', category: 'juice')
+Ingredient.create(name: 'pineapple juice', category: 'juice')
+Ingredient.create(name: 'apple juice', category: 'juice')
+
+pp Ingredient.all
+
+puts ""
+puts "> Done generating ingredients! 🥃"
