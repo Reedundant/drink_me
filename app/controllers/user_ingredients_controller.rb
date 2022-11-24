bbe33687f612828f1def3ec649c0df5b9442ce2d
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-24 14:17:43 -0500

    Verified create method with TA Chris, thank you

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -14,53 +14,25 @@ class UserIngredientsController < ApplicationController
   end
 
   def create
-    binding.break
-    # @user_ingredient.ingredient = Ingredient.find(23)
-    @user_ingredient = UserIngredient.new(user_ingredient_params)
+    # binding.break
+    @user = current_user
 
     ids = params["user_ingredient"]["ingredient_id"]
-    filtered_ids = ids.select { |id| id.present? }
+    @ingredients = Ingredient.where(id: ids)
 
-    filtered_ids.each do |id|
-      @user_ingredient.ingredient = Ingredient.find(id)
+    # filtered_ids = ids.select { |id| id.present? }
+
+    @ingredients.each do |i|
+      @user_ingredient = UserIngredient.new(ingredient: i, user: @user)
+      render :new, status: 422 and return unless @user_ingredient.save
     end
 
-    @user_ingredient.user = current_user
-
-    if @user_ingredient.save
-      redirect_to new_user_ingredient_path, notice: "works!"
-    else
-      render :new, status: :unprocessable_entity
-    end
+    redirect_to user_ingredients_path, notice: "works!"
   end
 
   private
 
   def user_ingredient_params
     params.require(:user_ingredient).permit(:ingredient_id)
-    # binding.break
   end
-
-  # RAISE: private method `select' called for nil:NilClass
-  # def filter_ingredient_ids
-  #   user_ingredient_params["ingredient_id"].select { |id| id.present? }
-  # end
 end
-
- # Loop over the get_ingredient_ids, find ingredient that belongs to the id
-  # with found ingredient, CREATE NEW USER INGREDIENT
-  # UserIngredient.new({
-    # user: user goes here,
-    # ingredient: ingredient goes here
-  #})
-
-  # def fetch_ingredients
-  #   # binding.break
-  #   raise
-  #   # user_ingredient_params.each do |id|
-  #   #   Ingredient.find(id)
-  #   # end
-
-  #   # params["user_ingredient"]["ingredient_id"]
-  # end
-  # pp fetch_ingredients
