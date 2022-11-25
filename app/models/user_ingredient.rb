764fe84438329930dc19f4c249be1014006a017e
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-24 23:29:48 -0500

    Declare enum attribute in user ingredient model

--- app/models/user_ingredient.rb
+++ app/models/user_ingredient.rb
@@ -6,4 +6,9 @@ class UserIngredient < ApplicationRecord
   has_many :custom_recipes, through: :custom_recipe_user_ingredients
 
   validates :ingredient, presence: { strict: true }
+
+  enum :status, {
+    unselected: 0, # .unselected => UserIngredient.where(status: :unselected)
+    selected: 1
+  }
 end
