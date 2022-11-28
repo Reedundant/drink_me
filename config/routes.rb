dc27aa2eb5e4f61ce3ea0e4d530e71435bc5ef79
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-28 12:25:09 -0500

    This is the big one. Routes have been pruned, migrations have been made, and we're about to find the mother of all bugs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :user_ingredients, only: %i[index new create]
   resources :user_ingredients, except: :show
-  resources :user_tools, only:[:new, :create, :index]
+  # resources :user_tools, only:[:new, :create, :index]
   get 'pages/suggestion', to: "pages#suggestion", as: :suggestion
 
   get 'pages/results', to: "pages#results", as: :results
 
-  resources :user_tools, only: %I[new create index]
+  # resources :user_tools, only: %I[new create index]
 
   get '/bookings/unselect', to: 'user_ingredients#unselect', as: :unselect_ingredient
   get '/bookings/select', to: 'user_ingredients#select', as: :select_ingredient
 
   get 'recipes/under_your_nose', to: 'recipes#under_your_nose', as: :under_your_nose
 
-  resources :recipes, only: :show do
-    resources :custom_recipes, only: %I[show new create]
-  end
-
+  resources :recipes, only: %i[show index create]
 end
