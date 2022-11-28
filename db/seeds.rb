af9688c455301aa7c958cab843e543b76439de4b
melissa banoen garde <m.banoengarde@gmail.com>
2022-11-28 15:40:13 -0500

    Remove category column from ingredients table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require_relative 'api_call_ingredients'
+
 puts "> Destroying all ingredients, and users..."
 Recipe.destroy_all
 UserIngredient.destroy_all
@@ -12,22 +14,17 @@ puts ""
 User.create!(email: 'jacktorrance@gmail.com', username: 'jacktorrance', age: 57, password: 'drinkme123')
 User.create!(email: 'jacknicholson@gmail.com', username: 'jacknietzscheson', age: 85, password: 'drinkme123')
 # pp User.all
-
 puts "> Done generating users! 👤"
 puts ""
 
 puts "> Generating ingredients..."
 puts ""
 
-require_relative 'api_call_ingredients'
-
-# Call method from api_call_ingredients ()
+# Call method from api_call_ingredients
 api_call_ingredients
 
 # pp Ingredient.count
-
 puts ""
 puts "> Done generating ingredients! 🥃"
-
 
 # Recipe.create!(name: "Under Your Nose", recipe_type: recipetype)
