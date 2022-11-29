8bb13cd68013a75c26d9a2fa730441e17fdda86b
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-29 10:18:27 -0500

    working on AJAX prevent default

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_165124) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_203927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
