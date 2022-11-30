d2971cea1705dac4881c0ce53aaca9f68f910496
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-30 18:01:13 -0500

    Hopefully the final set up for our url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["https://drink-me-reedundant.herokuapp.com/"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
 end
