4c1c34687180e2e81991c91d09f1c3818491d7c9
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-12-01 14:15:44 -0500

    SPEED FIX

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -3,13 +3,11 @@ class UserIngredientsController < ApplicationController
   before_action :create_user_ingredients, only: [:index]
 
   def index
-    # @ingredients = Ingredient.where(user: current_user)
-    ## Add .order to help organize ingredients
+    @user = current_user
     @user_ingredients = UserIngredient.where(user: current_user).includes(:ingredient).order('ingredients.name asc')
   end
 
   def toggle_selected
-    # ingredient.toggle :selected
     @user_ingredient = UserIngredient.find(params[:id])
 
     if @user_ingredient.selected
@@ -19,8 +17,6 @@ class UserIngredientsController < ApplicationController
     end
 
     head :ok
-    # redirect_to user_ingredients_path
-    # raise
   end
 
   private
