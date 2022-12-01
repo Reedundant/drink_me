04b4fb5239cdd07ee1f5d80629d569bdc0b565c0
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-12-01 11:12:42 -0500

    Hotfix is good to go!

--- app/javascript/controllers/prevent_reload_on_select_controller.js
+++ app/javascript/controllers/prevent_reload_on_select_controller.js
@@ -13,14 +13,16 @@ export default class extends Controller {
 
   prevent(event) {
     event.preventDefault()
+
+    if (document.querySelectorAll('.selected').length < 5){
     this.cardTarget.classList.toggle('selected')
     const url = `${window.location.href}/${this.cardTarget.id}/toggle_selected`
-    // console.log(document.querySelectorAll('.selected').length)
     fetch(url)
-    if (document.querySelectorAll('.selected').length > 5){
-      // console.log(document.querySelectorAll('.card'));
-      // console.log(document.querySelectorAll('.selected')[5]);
-      document.querySelectorAll('.selected')[5].classList.remove('selected')
+      // document.querySelectorAll('.selected')[5].classList.remove('selected'
+    } else if (document.querySelectorAll('.selected').length == 5 && this.cardTarget.classList.contains('selected')){
+      this.cardTarget.classList.toggle('selected')
+      const url = `${window.location.href}/${this.cardTarget.id}/toggle_selected`
+      fetch(url)
     }
   }
 }
